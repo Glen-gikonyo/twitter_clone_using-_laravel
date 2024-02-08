792cb02e94cdc66446f1c1d46ff6c11963e12774
Glen-gikonyo <consultantcoder02@gmail.com>
2024-02-08 10:57:28 +0300

    Did a migration for ideas table and also added model and controller for adding ideas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,22 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = [
-            [
-                'name' => 'Alex',
-                'age' => '30',
-            ],
-            [
-                'name' => 'Dan',
-                'age' => '25'
-            ]
-        ];
-        return view('dashboard', ['users' => $users]);
+        // database entry
+        // $idea = new Idea([
+        //     'content' => "hello twitter or is it now known as X 😂"
+        // ]);
+        // $idea->save();
+
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at','DESC')->get()
+        ]);
     }
 }
