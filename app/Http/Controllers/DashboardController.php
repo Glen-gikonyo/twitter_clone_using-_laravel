3c3989b9d4eceedb5a3f7fad02e05923c9b0d8ae
Glen-gikonyo <consultantcoder02@gmail.com>
2024-02-13 01:29:41 +0300

    Added Post functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,10 +10,7 @@ class DashboardController extends Controller
     public function index()
     {
         // database entry
-        // $idea = new Idea([
-        //     'content' => "hello twitter or is it now known as X 😂"
-        // ]);
-        // $idea->save();
+       
 
         return view('dashboard', [
             'ideas' => Idea::orderBy('created_at','DESC')->get()
